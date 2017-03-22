96f059e544c14146d52583e56a158671eeb35099
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-03-22 11:33:36 -0400

    Will need a review if possible (maybe personal)

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,25 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+
+  def show
+     @guest = Guest.find(params[:id])
+  end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(strong_params)
+    @guest.save
+    # redirect guest_path(guest) 
+  end
+
+  private
+    def strong_params
+      params.require(:guest).permit(:name, :occupation)
+    end
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,7 @@
 class Guest < ApplicationRecord
+  belongs_to :episode
+  has_many :episodes
+  #need to express the possibility that
+  #there a Guest has one episdode.
 
 end
